649c3dfcc57c6ccd1e9bf2c98954c9e4322d8814
grYe99 <guorongye99@gmail.com>
2026-04-25 21:30:27 +0800

    update: get_num_video_tokens
    Signed-off-by: grYe99 <guorongye99@gmail.com>

--- vllm/model_executor/models/glm4_1v.py
+++ vllm/model_executor/models/glm4_1v.py
@@ -1251,12 +1251,11 @@ class Glm4vProcessingInfo(BaseProcessingInfo):
         image_height: int,
         num_frames: int,
     ) -> int:
-        video_processor = self.get_video_processor()
         _, num_video_tokens = self._get_vision_info(
             image_width=image_width,
             image_height=image_height,
             num_frames=num_frames,
-            max_image_pixels=video_processor.size["longest_edge"],
+            max_image_pixels=self._get_image_max_pixels(),
         )
         return num_video_tokens
 
